098fc268036b8aac34592abbc327532aa7847bd1
awaisspk <iamawaisiqball@gmail.com>
2021-12-05 18:11:44 +0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import {Button} from '@components/Button';
+import type {NextPage} from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Button>button</Button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,7 @@
+import {styled} from '@stitches';
+import {motion} from 'framer-motion';
+
+export const Button = styled(motion.button, {
+  all : 'unset',
+  backgroundColor: '$pink',
+});
